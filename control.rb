ad5065dccfd497f2df886c351ac8973aa3c6b2c3
Yung Dai <yungchidai@gmail.com>
2014-10-21 11:54:28 -0400

    Ruby Lessons

---
+++ control.rb
@@ -0,0 +1,67 @@
+#Times
+
+# 5.times do 
+# 	puts "Hello world!"
+# end
+
+# same as 
+
+# 5.times {puts "hello world!"}
+
+# each
+
+# one_to_ten = (1..10)
+
+# one_to_ten.each do |number|
+# 	puts number
+# end
+
+# |variable| inside of the | is only valid inside the loop
+# and not outside of it.
+
+
+#Until
+# counter = 0
+
+# until counter == 4
+# 	puts "Count up: #{counter}"
+# 	counter += 1
+# end
+
+# While loop
+
+# counter = 0 
+# while counter < 4
+# 	puts "Counting up to #{counter}"
+# 	counter +=1
+# end
+
+
+# this is an inline statement for put 
+
+# x = 3
+# y = 5
+
+# puts "x is great than y!" if x > y
+
+
+# x = 3
+# y = 5
+
+# if x > y
+# 	puts "x is greater than y!"
+# elseif x < y
+# 	puts "x is lesser than y!"
+# else
+# 	puts "x is equal to y"
+# end
+
+
+
+#condition = true
+
+
+#if condition
+#	puts "I get printed!"
+#end
+
